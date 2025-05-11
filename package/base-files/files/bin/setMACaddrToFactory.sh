ea9aa4754cc569dc6460d329a7522322b3252a15
kshan <kshan@daejinnetworks.com>
2025-05-11 01:51:22 -0700

    Add script file for write MAC address to factory flash

---
+++ package/base-files/files/bin/setMACaddrToFactory.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get ethaddr by fw_printenv
+dump_ethaddr=$(/usr/sbin/fw_printenv | grep '^ethaddr' | tr '\n' ' ')
+ethaddr=${dump_ethaddr#*ethaddr=}
+ethaddr=${ethaddr%% *}
+ethaddr1=${dump_ethaddr#*ethaddr1=}
+
+# Converter ':' to space
+# e.g.: 1C:56:8E:2F:E4:EE -> 1C 56 8E 2F E4 EE
+wan_mac=${ethaddr//:/ }
+
+# Set WAN/LAN MAC address by mtk_factory_rw.sh
+echo "Setting WAN MAC address: $wan_mac"
+/sbin/mtk_factory_rw.sh -w wan $wan_mac
+
